1bc8f94568429442838da0118855a00892c345f6
simon231442 <simon2314@hotmail.com>
2026-03-28 07:36:50 +0100

    lenovo : ex01 commence

---
+++ ex01/Form.cpp
@@ -0,0 +1,14 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+class Form {
+	private :
+		std::string	Name_;
+		bool		Signed_;
+		int const	RequiredGradeToSigned_;
+		int const	RequiredGradeToExec_;
+	public :
+		Form(std::string const name,
+		void		beSigned();
+
+
